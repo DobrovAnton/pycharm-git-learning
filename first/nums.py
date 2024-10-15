37f0950708203b4a600ec8895be6d90d8a16d240
Dobrov Anton <antdobrov@gmail.com>
2024-10-15 15:43:20 +0700

    Добавлена новая функциональность

--- first/nums.py
+++ first/nums.py
@@ -1,10 +1,19 @@
 def plus(a, b):
     return a + b
 
+
 text = "new text"
+
 
 def minus(a, b):
     return a - b
 
+
+def mul(a, b):
+    return a * b
+
+
 if __name__ == '__main__':
     print(plus(2, 2))
+    print(minus(7, 3))
+    print(mul(8, 8))
